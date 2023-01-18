f1e302582af300ad6ae44ee28e0f56ab3b56e0f0
Jesse Florig <jesseflorig@gmail.com>
2023-01-18 14:37:13 +0100

    changed bg to scheme

--- src/Door.js
+++ src/Door.js
@@ -11,11 +11,11 @@ export default function Door({label, topic}) {
   const doorIcon = open ? <RiDoorOpenFill /> : <RiDoorFill />
 
   const locked = message?.payload.locked
-  const background = locked ? "blue.500" : "gray.600"
+  const scheme = locked ? "blue" : null
   const toggleValue = locked ? 0 : 1
   const lockIcon = locked ? <FaLock /> : <FaUnlock />
 
   return (
-    <PublishButton leftIcon={lockIcon} rightIcon={doorIcon} label={label} bg={background} topic={topic} message={{locked: toggleValue}} />
+    <PublishButton leftIcon={lockIcon} rightIcon={doorIcon} label={label} colorScheme={scheme} topic={topic} message={{locked: toggleValue}} />
   );
 }
