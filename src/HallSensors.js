a003868342720e57642109c5151f96b7127e0644
Jesse Florig <jesseflorig@gmail.com>
2024-05-11 01:37:46 -0400

    updated office topics

--- src/HallSensors.js
+++ src/HallSensors.js
@@ -1,9 +1,11 @@
+import { HStack } from '@chakra-ui/react'
 import Light from './Light'
 
 export default function OfficeSensors(){
   return (
-    <>
-    <Light label="Pico" topic="pico/light/" />
-    </>
+    <HStack>
+      <Light label="Pico" topic="fleet1/pico/light/" />
+      <Light label="LED" topic="fleet1/pico/light2/" />
+    </HStack>
   )
 }
